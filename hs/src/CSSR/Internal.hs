5092e0819f662d59bac6b5fda0ba01f325b63e49
stites <sam@stites.io>
2017-09-08 00:19:11 -0400

    remove internal module

--- hs/src/CSSR/Internal.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE FlexibleInstances #-}
-module CSSR.Internal where
-
-import Data.HashMap.Strict (HashMap)
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Data.Hashable
-
-
