35755868c0bcef631cfa7d4105080b0535a16f10
stites <sam@stites.io>
2017-10-17 20:44:45 -0400

    import variation file

---
+++ hs/src/CSSR/Statistics/Variation.hs
@@ -0,0 +1,17 @@
+module CSSR.Statistics.Variation where
+
+import CSSR.Prelude
+import qualified Data.Tree.Hist as Hist
+import qualified Data.Vector as V
+
+
+type InferredDistribution = [(Hist.Leaf, Double)]
+
+variation :: InferredDistribution -> Double -> Double
+variation dist adjustedDataSize = foldl' go 0 dist
+  where
+    go :: Double -> (Hist.Leaf, Double) -> Double
+    go total (h, inferredProb) = total + abs (historyProb - inferredProb)
+      where
+        historyProb :: Double
+        historyProb = V.sum $ V.map ((/ adjustedDataSize).fromIntegral) (view Hist.lfrequencyL h)
