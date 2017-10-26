77396ddd725727b3a04deae8b0a88b443571da46
stites <sam@stites.io>
2017-10-25 22:13:46 -0400

    test growing the looping tree

--- hs/src/CSSR/Algorithm/Phase2.hs
+++ hs/src/CSSR/Algorithm/Phase2.hs
@@ -20,58 +20,32 @@
 -- ENDWHILE
 -------------------------------------------------------------------------------
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE LambdaCase #-}
 module CSSR.Algorithm.Phase2 where
 
-import CSSR.Prelude
-import Data.MTree.Looping as ML
-import Data.Maybe
-import Data.Tree.Looping as L hiding (excisable, homogeneous)
-import qualified CSSR.Prelude.Vector as V
-import qualified CSSR.Probabilistic as Prob
 import qualified Data.Text as T
-import qualified Data.Tree.Conditional as Cond
 import qualified Data.Sequence as S
-import qualified Data.List.Set as Set
 import qualified Data.HashSet as HS
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashTable.Class as H
 
 import CSSR.Prelude.Mutable
--- for doctest
-import CSSR.Algorithm.Phase1
+import Data.MTree.Looping as ML
+import Data.Tree.Looping as L hiding (excisable, homogeneous)
+
+import qualified CSSR.Prelude.Vector as V
+import qualified CSSR.Probabilistic as Prob
+import qualified Data.List.Set as Set
+import qualified Data.Tree.Conditional as Cond
 
 
--- |
--- Example:
---
--- >>> import CSSR.Algorithm.Phase1
--- >>> let short_ep = "00011110001100011110000111101101111111111000110001101101100111100111100"
--- >>> let htree = initialization 1 short_ep
--- >>> runST $ grow 0.01 htree >>= ML.freezeTree
--- Leaf{fromList [
---      " "->Leaf{obs: [], freq: [28,42]}
---           children:
---           "0"->Leaf{obs: ["0"], freq: [1,1], no children}
--- <BLANKLINE>
---           "1"->Leaf{obs: ["1"], freq: [1,1], no children}], [0,0], fromList []}
 grow :: forall s . Double -> Cond.Tree -> ST s (ML.MTree s)
-grow sig htree = do
-  (rt, ts, q) <- queueRoot sig htree
+grow sig (Cond.Tree _ a hRoot) = do
+  rt <- ML.mkRoot a hRoot              -- INIT root looping node
+  ts <- newSTRef $ fromList [rt]       -- INIT queue of active, unchecked nodes
+  let q = S.singleton rt               -- QUEUE root
   findTerminals sig q ts
   pure $ ML.MTree ts rt
-
-
--- INIT root looping node
--- INIT queue of active, unchecked nodes
--- QUEUE root
-queueRoot :: forall s . Double -> Cond.Tree -> ST s (MLeaf s, STRef s (Set.ListSet (MLeaf s)), Seq (MLeaf s))
-queueRoot sig (Cond.Tree _ a hRoot) = do
-  rt <- ML.mkRoot a hRoot   -- INIT root looping node
-  ts <- newSTRef $ fromList [rt]       -- INIT queue of active, unchecked nodes
-  let q = S.singleton rt    -- QUEUE root
-  return (rt, ts, q)
 
 
 -- WHILE queue is not empty
@@ -125,34 +99,4 @@ nextChilds active = (ML.childHistories >=> mapM activeAsLeaf . groupHistory) act
 
     groupHistory :: [Cond.Leaf] -> [(Event, [Cond.Leaf])]
     groupHistory = groupBy (fromMaybe "" . V.head . view (Cond.bodyL . Cond.obsL))
-
-
--- ========================================================================= --
-
-test1 :: Double -> Cond.Tree -> ST s L.Tree
-test1 sig htree = do
-  (rt, ts, q) <- queueRoot sig htree
-  findTerminals sig q ts
-  cs::[(Event,MLNode s)] <- H.toList (ML.children rt)
-  let go :: (Event, MLNode s) -> ST s Int
-      go (e, n) =
-        case n of
-          Left loop -> pure (-1)
-          Right c -> do
-            hs' <- readSTRef (ML.histories c)
-            pure . HS.size $ hs'
-
-  hls :: [Int] <- mapM go cs
-  traceM $ "hls : " <> show hls
-
-  t <- ML.freezeTree $ ML.MTree ts rt
-  pure t
-
-maine :: IO ()
-maine = do
-  con <- readFile "../../test-machines/tmp/ep/EP_timeseq"
-  let hs = initialization 5 con
-  let x = runST $ test1 0.01 hs
-  print x
-
 
--- hs/test/CSSR/Algorithm/Phase2Spec.hs
+++ hs/test/CSSR/Algorithm/Phase2Spec.hs
@@ -3,6 +3,7 @@ module CSSR.Algorithm.Phase2Spec where
 import CSSR.Prelude.Test
 import CSSR.Prelude.Mutable (runST)
 
+import Control.Applicative ((<|>))
 import qualified Data.HashSet as HS
 import qualified Data.Vector as V
 import qualified Data.HashMap.Strict as HM
@@ -24,16 +25,34 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "a short even process root" $ do
-    let ltree = runST $ grow 0.01 (initialization 1 longEP) >>= ML.freezeTree
+  describe "a long even process root" $ do
+    let ltree = runST $ grow 0.01 (initialization 3 longEP) >>= ML.freezeTree
 
     describe "the root looping node" $ do
       it "should have the expected frequency" $
         Prob.freqToDist <$> preview (L.rootL . L.bodyL . _Right . L.frequencyL) ltree `shouldSatisfy`
-          maybe False (isApprox 0.05 (V.fromList [0.33,0.66]))
+          maybe False (isApprox 0.05 (V.fromList [1/3, 2/3]))
 
       context "its histories" $
-        historiesMatchSpec "root" (L.root ltree) [([0.33,0.66], "")]
+        historiesMatchSpec "root" (L.root ltree) [([1/3, 2/3], "")]
+
+    let d1 = maybe [] HM.elems $ preview (L.rootL . L.childrenL) ltree :: [L.Leaf]
+    describe "depth 1" $ depthSpec d1
+      [ Right ([1/2, 1/2], "0", [])
+      , Right ([1/4, 3/4], "1", ["1", "0"])
+      ]
+
+    let d2 = foldMap (HM.elems . L.children) d1
+    describe "depth 2" $ depthSpec d2
+      [ Left ""
+      , Right ([1/4, 3/4], "10", ["1", "0"])
+      ]
+
+    let d3 = foldMap (HM.elems . L.children) d2
+    describe "depth 3" $ depthSpec d3
+      [ Right ([1/2, 1/2], "101", [])
+      , Right ([1/4, 3/4], "100", [])
+      ]
 
   where
     toCheckableHists :: L.LeafBody -> HashSet (Vector Double, Vector Event)
@@ -42,13 +61,56 @@ spec =
     historiesMatchSpec :: Text -> L.Leaf -> [([Double], Text)] -> Spec
     historiesMatchSpec t ll = mapM_ $ \(dist, txtObs) ->
       it ("should have a history of " <> T.unpack txtObs <> " with distribution: " <> show dist) $
-        (check >>= find ((== txt2event txtObs) . snd)) `shouldSatisfy`
+        (tocheck >>= find ((== txt2event txtObs) . snd)) `shouldSatisfy`
             maybe False (isApprox 0.05 (V.fromList dist) . fst)
       where
-        check :: Maybe [(Vector Double, Vector Event)]
-        check = toCheckableHists <$> preview (L.bodyL . _Right . L.historiesL) ll
+        tocheck :: Maybe [(Vector Double, Vector Event)]
+        tocheck = toCheckableHists <$> preview (L.bodyL . _Right . L.historiesL) ll
 
         toCheckableHists :: HashSet Cond.Leaf -> [(Vector Double, Vector Event)]
         toCheckableHists = HS.toList . HS.map ((Prob.freqToDist . view Cond.lfrequencyL) &&& view Cond.lobsL)
+
+    depthSpec :: [L.Leaf] -> [Either Text ([Double], Text, [Event])] -> Spec
+    depthSpec ls = mapM_ tester
+     where
+      tester :: Either Text ([Double], Text, [Event]) -> Spec
+      tester (Left loop) =
+        it ("should have a loop to " <> show loop) $
+          foldr (finder loop . tocheckable) False ls `shouldBe` True
+       where
+        finder
+          :: Text -> Either (Vector Event) (Vector Double, [Vector Event], [Event]) -> Bool -> Bool
+        finder tobs (Left x)  memo = memo || txt2event tobs == x
+        finder _    (Right _) memo = memo
+
+
+      tester (Right (dist, tobs, nxts)) =
+        it ("should have a leaf " <> show tobs <> " with distribution: " <> show dist
+            <> " and children at " <> show nxts) $
+          foldr (finder tobs . tocheckable) Nothing ls `shouldSatisfy`
+                maybe False (predicate dist nxts)
+       where
+        finder
+          :: Text -> Either (Vector Event) (Vector Double, [Vector Event], [Event])
+          -> Maybe (Vector Double, [Vector Event], [Event])
+          -> Maybe (Vector Double, [Vector Event], [Event])
+        finder _    (Left _)  memo = memo
+        finder tobs (Right x) memo = memo
+          <|> maybe Nothing (const $ Just x) (find (== txt2event tobs) (view _2 x))
+
+
+      tocheckable :: L.Leaf -> Either (Vector Event) (Vector Double, [Vector Event], [Event])
+      tocheckable l = case L.body l of
+        Left (L.LeafRep os) -> Left os
+        Right bod -> Right (bod2dist bod, bod2obs bod, HM.keys (L.children l))
+       where
+        bod2dist :: L.LeafBody -> Vector Double
+        bod2dist =Prob.freqToDist . L.frequency
+
+        bod2obs :: L.LeafBody -> [Vector Event]
+        bod2obs = map (view Cond.lobsL) . HS.toList . L.histories
+
+      predicate :: [Double] -> [Event] -> (Vector Double, [Vector Event], [Event]) -> Bool
+      predicate d0 cs0 (d1, _, cs1) = isApprox 0.05 (V.fromList d0) d1 && all (`elem` cs1) cs0
 
 
