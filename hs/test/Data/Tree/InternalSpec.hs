3ef0fd61ccaa1497f5d94a13b16b7983ccee0fbb
stites <sam@stites.io>
2017-11-05 10:40:35 -0500

    add more tests around getAncestors

--- hs/test/Data/Tree/InternalSpec.hs
+++ hs/test/Data/Tree/InternalSpec.hs
@@ -8,14 +8,38 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "getAncestors on 0-100, 0 being oldest" $ do
-    let ancestors = getAncestors mkParent 100
-    it "should not include itself" $
-      ancestors `shouldNotContain` [100]
-    it "should be in order of oldest last" $
-      take 10 ancestors `shouldBe` [0..9]
+  describe "getAncestors" $ do
     it "should return the empty list when no parent is found" $
       getAncestors (const Nothing) (undefined::Int) `shouldBe` []
-  where
-    mkParent :: Int -> Maybe Int
-    mkParent i = if i <= 0 then Nothing else Just (i-1)
+
+    describe "0-100, 0 being oldest" $ do
+      let
+        mkParent :: Int -> Maybe Int
+        mkParent i = if i <= 0 then Nothing else Just (i-1)
+
+        ancestors :: [Int]
+        ancestors = getAncestors mkParent 100
+
+      it "should not include itself" $
+        ancestors `shouldNotContain` [100]
+      it "should be in order of oldest last" $
+        take 10 ancestors `shouldBe` [0..9]
+
+    describe "on [[],[1],[1,2],[1,2,3]..[1..10]], [] being oldest" $ do
+      let
+        mkParent :: [Int] -> Maybe [Int]
+        mkParent   [] = Nothing
+        mkParent ints = Just $ take (length ints - 1) ints
+
+        youngest :: [Int]
+        youngest = [1..10]
+
+        ancestors :: [[Int]]
+        ancestors = getAncestors mkParent youngest
+
+      it "should not include itself" $
+        ancestors `shouldNotContain` [youngest]
+
+      it "should be in order of oldest last" $
+        take 3 ancestors `shouldBe` [[], [1], [1,2]]
+
