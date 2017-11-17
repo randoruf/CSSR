5f5732212a59d67521bd695478e0d8221f5d4859
stites <sam@stites.io>
2017-11-16 20:45:00 -0500

    generate inferred distributions

--- hs/src/CSSR/Inferred.hs
+++ hs/src/CSSR/Inferred.hs
@@ -1,91 +1,55 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module CSSR.Inferred where
 
+import CSSR.AllStates
 import CSSR.Prelude
 import Data.Tree.Conditional (lobsL)
+
 import qualified Data.Tree.Conditional as Cond
+import qualified Data.Tree.Looping as L
+import qualified Data.Vector as V
+import qualified Data.HashMap.Strict as HM
 
-type State = ()
-type AllStates = [State]
+type LLeaf = L.Leaf
 
--- | calculates the probability of all the histories up to length/depth indicated, based on a given allStates
+-- | calculates the probability of all the histories up to length/depth indicated, based
+-- on a given allStates
 inferredDistribution :: Cond.Tree -> Int -> AllStates -> [(Cond.Leaf, Double)]
 inferredDistribution tree@Cond.Tree{Cond.depth} d allstates =
-  fmap (\h -> (h, inferredCond (view lobsL h) tree allstates)) (tree `Cond.getDepth` depth)
+  fmap (identity &&& inferredProb tree allstates . view lobsL)
+    (tree `Cond.getDepth` depth)
 
--- | calculates the probability of a single, raw history (in string form) based on a given allStates and alphabet
-inferredHist :: Vector Event -> Cond.Tree -> AllStates -> Double
-inferredHist hist tree allstates = foldr totalPerString 0 (zip [0..] allstates)
-  where
-    totalPerString :: (Integer, State) -> Double -> Double
-    totalPerString = undefined
-{-
-object InferProbabilities extends Logging {
-  override def loglevel() = Level.OFF
+-- | calculates the probability of a single, raw history (in string form) based on a given
+-- allStates and alphabet
+inferredProb :: Cond.Tree -> AllStates -> Vector Event -> Double
+inferredProb tree allstates hist = sum (totalPerString <$> allstates)
+ where
+  dist :: State -> HashMap Event Double
+  dist = distributionsLookup (Cond.alphabet tree) allstates
 
-  type InferredDistribution = Array[(ParseLeaf, Double)]
+  allStatesDist :: State -> Double
+  allStatesDist = distributionLookup (Cond.alphabet tree) allstates
 
-  /**
-    * calculates the probability of all the histories up to length/depth indicated, based on a given allStates
-    */
-  def inferredDistribution(tree: ParseTree, depth:Int, allStates: AllStates):InferredDistribution = {
-    val inferred = tree
-      .getDepth(depth)
-      .map { h => (h, inferredHistory(h.observed, tree, allStates) ) }
+  totalPerString :: State -> Double
+  totalPerString start = fst (V.foldl' go (1, start) hist) * allStatesDist start
+   where
+    go :: (Double, State) -> Event -> (Double, State)
+    go (charTotalPerState, current) e =
+      case current `transitionNonNull` e of
+        -- FIXME: because there exists a null states within misuriwicz
+        -- (history lengths are too long?), this is currently destroying results
+        Nothing -> (0, current)
+        Just t  -> (HM.lookupDefault 0 e (dist current) * charTotalPerState, t)
 
-    debug(s"inferred distribution total: ${inferred.map{_._2}.sum}")
-    debug(s"inferred distribution size: ${inferred.length}")
-    // inferred.map{ case (l, p) => (l.observed, p) }.foreach{ i => println(i) }
+    transitionNonNull :: State -> Event -> Maybe State
+    transitionNonNull s e = do
+      t <- s `transitionTo` e
+      guard (notNull s)
+      pure t
 
-    inferred
-  }
-
-  /**
-    * calculates the probability of a single, raw history (in string form) based on a given allStates and alphabet
-    */
-  def inferredHistory(history:List[Event], tree: ParseTree, allStates: AllStates): Double = {
-    // FIXME: this would be perfect to replace with a state monad
-    info("Generating Inferred probabilities from State Machine")
-
-    val totalPerString = allStates.states
-      .view
-      .zipWithIndex
-      .map {
-        case (state, i) =>
-          debug(s"$history - STATE ${i.toString} {frequency:${allStates.distribution(i)}}")
-          var currentStateIdx = i
-          var isNullState = false
-
-          val historyTotalPerState = history
-            .foldLeft(1d) {
-            (characterTotalPerState, c) => {
-              val currentState = allStates.states(currentStateIdx)
-              var transitionState:TransitionState = None
-              try {
-                transitionState = allStates.transitions(currentStateIdx)(c)
-              } catch {
-                case e: Exception => throw new Exception("failing alphabet symbol is " + c, e)
-              }
-              isNullState = isNullState || transitionState.isEmpty
-
-              if (isNullState) {
-                0d /// FIXME: because there exists a null states within misuriwicz (history lengths are too long?), this is currently destroying results
-              } else {
-                currentStateIdx  = allStates.states.zipWithIndex.find(_._1 == transitionState.get).get._2
-                val totalPerStateCached = characterTotalPerState * currentState.distribution(tree.alphabet.map(c))
-                totalPerStateCached
-              }
-            }
-          }
-          debug(s"final historyTotalPerState: $historyTotalPerState")
-          allStates.distribution(i) * historyTotalPerState
-      }.sum[Double]
-
-    debug(s"Final Probability for History: $totalPerString")
-    totalPerString
-
-    /*
-    // TODO: ask about this: seems like we are just hitting a steady-state on every history. is this normal? if so, it looks like we are double-ish counting.
+    {-
+    // TODO: ask about this scala code: seems like we are just hitting a steady-state on every
+    // history. is this normal? if so, it looks like we are double-ish counting.
     leaf.observed.reverse.view.zipWithIndex.foldLeft[Double](1d){
       (totalPerState, pair) => {
         val (c, i) = pair
@@ -93,8 +57,6 @@ object InferProbabilities extends Logging {
         val currentState = current.get.currentEquivalenceClass
         val next = current.get.findChildWithAdditionalHistory(c)
         val nextEqClassIdx = allStates.states.indexOf(next.get.currentEquivalenceClass)
-
-        println(totalPerState, c, current.get.observed, next.get.observed, leaf.observed, nextEqClassIdx)
 
         current = next
         if (!allStates.states.contains(next.get.currentEquivalenceClass)) {
@@ -104,8 +66,5 @@ object InferProbabilities extends Logging {
         }
       }
     }
-    */
-  }
+    -}
 
-}
--}
