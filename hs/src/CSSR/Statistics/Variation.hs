c3ddead9c50aab8708ab0f2f2df276874a6d903f
stites <sam@stites.io>
2017-11-27 14:49:47 -0500

    update variation

--- hs/src/CSSR/Statistics/Variation.hs
+++ hs/src/CSSR/Statistics/Variation.hs
@@ -1,16 +1,20 @@
 module CSSR.Statistics.Variation where
 
 import CSSR.Prelude
-import qualified Data.Tree.Conditional as Cond
+import CSSR.AllStates (State, terminal)
+import qualified Data.Tree.Looping as L
 import qualified Data.Vector as V
+import qualified Data.HashMap.Strict as HM (toList)
 
-type InferredDistribution = [(Cond.Leaf, Double)]
+variation :: HashMap State Double -> Double -> Double
+variation dist adjustedDataSize = foldl' go 0 (HM.toList dist)
+ where
+  go :: Double -> (State, Double) -> Double
+  go total (h, inferredProb) = total + abs (historyProb - inferredProb)
+   where
+    historyProb :: Double
+    historyProb = V.sum $ V.map ((/ adjustedDataSize).fromIntegral) (viewFrequency h)
 
-variation :: InferredDistribution -> Double -> Double
-variation dist adjustedDataSize = foldl' go 0 dist
-  where
-    go :: Double -> (Cond.Leaf, Double) -> Double
-    go total (h, inferredProb) = total + abs (historyProb - inferredProb)
-      where
-        historyProb :: Double
-        historyProb = V.sum $ V.map ((/ adjustedDataSize).fromIntegral) (view Cond.lfrequencyL h)
+    viewFrequency :: State -> Vector Integer
+    viewFrequency s = terminal s ^. (L.bodyL . _Right . L.frequencyL)
+
